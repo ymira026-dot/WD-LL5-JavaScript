6fd170fbbbe31d81e8361c93bee76ecc825f17bf
Yelysavet Miranda <ymira026@fiu.edu>
2026-02-20 01:07:31 +0000

    add initial HTML and JavaScript files with basic logging and alerts

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log ("Hello, World!"); //Direct log
+console.error("This is my first JavaScript error!"); //error log
+
+//alert logs
+alert("Yelysavet");
+alert("Sonseray");
+alert("Caleb");
+alert("Paul");
+alert("Kiet");
+
+//stretch
+console.log(console);
